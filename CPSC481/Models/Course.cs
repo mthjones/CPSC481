eba107ab855e9df9ee60347832f7be41ae2db2d8
Matt Jones <m.t.h.jones@gmail.com>
2012-11-17 15:57:17 -0700

    Created Course model.

---
+++ CPSC481/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPSC481.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+    }
+}
