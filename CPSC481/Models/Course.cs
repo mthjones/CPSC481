54847b37582da1623630e3eb70dd99de2f894609
Matt Jones <m.t.h.jones@gmail.com>
2012-11-17 19:16:01 -0700

    Added references to other models to course model.

--- CPSC481/Models/Course.cs
+++ CPSC481/Models/Course.cs
@@ -8,5 +8,9 @@ namespace CPSC481.Models
     public class Course
     {
         public string Name { get; set; }
+        public List<Announcement> Announcements { get; set; }
+        public List<Thread> Threads { get; set; }
+        public List<Lecture> Lectures { get; set; }
+        public List<Assignment> Assignments { get; set; }
     }
 }
