e5afc07250359e59f0c35a018111f20fd8c2c1cd
Matt Jones <m.t.h.jones@gmail.com>
2012-11-17 20:32:36 -0700

    Changed main view model collections to obersvable collections.

--- CPSC481/ViewModels/MainViewModel.cs
+++ CPSC481/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CPSC481.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -8,19 +9,19 @@ namespace CPSC481.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        private IEnumerable<string> _secondaryCategories;
-        private IEnumerable<string> _primaryCategories;
+        private ObservableCollection<string> _secondaryCategories;
+        private ObservableCollection<string> _primaryCategories;
         private Course _course;
 
         public MainViewModel()
         {
-            PrimaryCategories = new List<string> { "Announcements", "Forums", "Lectures", "Assignments" };
-            SecondaryCategories = new List<string> { };
+            PrimaryCategories = new ObservableCollection<string> { "Announcements", "Forums", "Lectures", "Assignments" };
+            SecondaryCategories = new ObservableCollection<string> { };
             Course = new Course { Name = "CPSC 481" };
-            Course.Announcements = new List<Announcement> { new Announcement() { Title="Lorem ipsum" }, new Announcement() { Title="Dolor sit amet" }, new Announcement() { Title="Hello world" }, new Announcement() { Title="Testing 1,2,3" } };
-            Course.Threads = new List<Thread> { new Thread() { Title = "Nullam Pellentesque" }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
-            Course.Lectures = new List<Lecture> { new Lecture() { Content = "Ligula" }, new Lecture() { Content = "Ultricies Amet Cras" }, new Lecture() { Content = "Euismod Purus" }, new Lecture() { Content = "Magna" }, new Lecture() { Content = "Vulputate Pharetra" } };
-            Course.Assignments = new List<Assignment> { new Assignment() { Content="Parturient Ipsum" }, new Assignment() { Content="Fringilla" }, new Assignment() { Content="Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Content="Fusce Ligula" }, new Assignment() { Content="Etiam Ullamcorper" } };
+            Course.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "Lorem ipsum" }, new Announcement() { Title = "Dolor sit amet" }, new Announcement() { Title = "Hello world" }, new Announcement() { Title = "Testing 1,2,3" } };
+            Course.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque" }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
+            Course.Lectures = new ObservableCollection<Lecture> { new Lecture() { Content = "Ligula" }, new Lecture() { Content = "Ultricies Amet Cras" }, new Lecture() { Content = "Euismod Purus" }, new Lecture() { Content = "Magna" }, new Lecture() { Content = "Vulputate Pharetra" } };
+            Course.Assignments = new ObservableCollection<Assignment> { new Assignment() { Content = "Parturient Ipsum" }, new Assignment() { Content = "Fringilla" }, new Assignment() { Content = "Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Content = "Fusce Ligula" }, new Assignment() { Content = "Etiam Ullamcorper" } };
         }
 
         public Course Course
@@ -28,12 +29,12 @@ namespace CPSC481.ViewModels
             get { return this._course; }
             set { NotifyPropertyChanged("Course"); this._course = value; }
         }
-        public IEnumerable<string> PrimaryCategories
+        public ObservableCollection<string> PrimaryCategories
         {
             get { return this._primaryCategories; }
             set { NotifyPropertyChanged("PrimaryCategories"); this._primaryCategories = value; }
         }
-        public IEnumerable<string> SecondaryCategories
+        public ObservableCollection<string> SecondaryCategories
         {
             get { return this._secondaryCategories; }
             set { NotifyPropertyChanged("SecondaryCategories"); this._secondaryCategories = value; }
