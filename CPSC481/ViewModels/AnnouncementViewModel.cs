20f65d6cc866d69be7f5669a6a4ca29b650c46a7
Matt Jones <m.t.h.jones@gmail.com>
2012-11-17 21:16:45 -0700

    Created AnnouncementView and ViewModel.

---
+++ CPSC481/ViewModels/AnnouncementViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CPSC481.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPSC481.ViewModels
+{
+    public class AnnouncementViewModel : ViewModelBase
+    {
+        private Announcement _announcement;
+        public Announcement Announcement
+        {
+            get { return this._announcement; }
+            set { NotifyPropertyChanged("Announcement"); this._announcement = value; }
+        }
+
+        public AnnouncementViewModel()
+        {
+            Announcement = new Announcement() { Title = "Announcement", Content = "Placeholder", Posted = DateTime.Now };
+        }
+    }
+}
