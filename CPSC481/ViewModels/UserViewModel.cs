19b1e1a6e5d8646463f71dabd05a2e5f7ed61d0f
Matt Jones <m.t.h.jones@gmail.com>
2012-11-18 15:51:04 -0700

    Created basic UserViewModel.

---
+++ CPSC481/ViewModels/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CPSC481.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPSC481.ViewModels
+{
+    public class UserViewModel : ViewModelBase
+    {
+        public UserViewModel()
+            : this(new User() { Username = "testuser" })
+        {
+        }
+
+        public UserViewModel(User user)
+        {
+            this.User = user;
+        }
+
+        private User User { get; set; }
+
+        public string Username
+        {
+            get { return User.Username; }
+            set { User.Username = value; NotifyPropertyChanged("Username"); }
+        }
+    }
+}
