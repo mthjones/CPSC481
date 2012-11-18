dfca52db5218153e575121ccc346d75de07b89d0
tygibb <tygibb@gmail.com>
2012-11-18 16:35:17 -0700

    added some content for announcements/threads

--- CPSC481/ViewModels/MainViewModel.cs
+++ CPSC481/ViewModels/MainViewModel.cs
@@ -35,22 +35,22 @@ namespace CPSC481.ViewModels
         private void LoadCourses()
         {
             Course CPSC481 = new Course() { Name = "CPSC 481" };
-            CPSC481.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "Lorem ipsum" }, new Announcement() { Title = "Dolor sit amet" }, new Announcement() { Title = "Hello world" }, new Announcement() { Title = "Testing 1,2,3" } };
-            CPSC481.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque" }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
+            CPSC481.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "Lorem ipsum",Content="The brown fox",Posted=System.DateTime.Now }, new Announcement() { Title = "Dolor sit amet",Content="jumps over",Posted=System.DateTime.Now }, new Announcement() { Title = "Hello world",Content="the lazy dog" }, new Announcement() { Title = "Testing 1,2,3" } };
+            CPSC481.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque",Content = "ullamco laboris nisi" }, new Thread() { Title = "Porta Ornare Venenatis",Content = ut aliquip ex }, new Thread() { Title = "Cras",Content="ea commodo consequat" } };
             CPSC481.Lectures = new ObservableCollection<Lecture> { new Lecture() { Content = "Ligula" }, new Lecture() { Content = "Ultricies Amet Cras" }, new Lecture() { Content = "Euismod Purus" }, new Lecture() { Content = "Magna" }, new Lecture() { Content = "Vulputate Pharetra" } };
             CPSC481.Assignments = new ObservableCollection<Assignment> { new Assignment() { Content = "Parturient Ipsum" }, new Assignment() { Content = "Fringilla" }, new Assignment() { Content = "Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Content = "Fusce Ligula" }, new Assignment() { Content = "Etiam Ullamcorper" } };
             Courses.Add(new CourseViewModel(CPSC481));
 
             Course CPSC457 = new Course() { Name = "CPSC 457" };
             CPSC457.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "Test ipsum" }, new Announcement() { Title = "Hello world" }, new Announcement() { Title = "Dolor" }, new Announcement() { Title = "Test" } };
-            CPSC457.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque" }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
+            CPSC457.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque",Content="Content here" }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
             CPSC457.Lectures = new ObservableCollection<Lecture> { new Lecture() { Content = "Ligula" }, new Lecture() { Content = "Ultricies Amet Cras" }, new Lecture() { Content = "Euismod Purus" }, new Lecture() { Content = "Magna" }, new Lecture() { Content = "Vulputate Pharetra" } };
             CPSC457.Assignments = new ObservableCollection<Assignment> { new Assignment() { Content = "Parturient Ipsum" }, new Assignment() { Content = "Fringilla" }, new Assignment() { Content = "Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Content = "Fusce Ligula" }, new Assignment() { Content = "Etiam Ullamcorper" } };
             Courses.Add(new CourseViewModel(CPSC457));
 
             Course SENG515 = new Course() { Name = "SENG 515" };
             SENG515.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "ipy ipsum" }, new Announcement() { Title = "Hey World" }, new Announcement() { Title = "Hello" }, new Announcement() { Title = "Testing" } };
-            SENG515.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque" }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
+            SENG515.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque",Content="words." }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
             SENG515.Lectures = new ObservableCollection<Lecture> { new Lecture() { Content = "Ligula" }, new Lecture() { Content = "Ultricies Amet Cras" }, new Lecture() { Content = "Euismod Purus" }, new Lecture() { Content = "Magna" }, new Lecture() { Content = "Vulputate Pharetra" } };
             SENG515.Assignments = new ObservableCollection<Assignment> { new Assignment() { Content = "Parturient Ipsum" }, new Assignment() { Content = "Fringilla" }, new Assignment() { Content = "Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Content = "Fusce Ligula" }, new Assignment() { Content = "Etiam Ullamcorper" } };
             Courses.Add(new CourseViewModel(SENG515));
