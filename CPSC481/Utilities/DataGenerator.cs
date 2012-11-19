7a0bd3ad424e9154341d6a90273367cf7bb97e6a
Matt Jones <m.t.h.jones@gmail.com>
2012-11-18 21:25:06 -0700

    Data now generated randomly.

---
+++ CPSC481/Utilities/DataGenerator.cs
@@ -0,0 +1,170 @@
+﻿using CPSC481.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace CPSC481.Utilities
+{
+    public class DataGenerator
+    {
+        private static Ipsum ipsum = new Ipsum();
+        private static Random rnd = new Random();
+
+        #region Announcements
+        private static List<Announcement> _announcements;
+        public static List<Announcement> Announcements
+        {
+            get { if (_announcements == null) GenerateAnnouncements(); return _announcements; }
+        }
+        private static void GenerateAnnouncements()
+        {
+            _announcements = new List<Announcement>();
+            for (int i = 0; i < 100; i++)
+            {
+                _announcements.Add(new Announcement() {
+                    Title = ipsum.GetWords(4),
+                    Content = ipsum.GetWords(100),
+                    Posted = GetRandomDateTime()
+                });
+            }
+        }
+        #endregion
+
+        #region Assignments
+        private static List<Assignment> _assignments;
+        public static List<Assignment> Assignments
+        {
+            get { if (_assignments == null) GenerateAssignments(); return _assignments; }
+        }
+        private static void GenerateAssignments()
+        {
+            _assignments = new List<Assignment>();
+            for (int i = 0; i < 100; i++)
+            {
+                _assignments.Add(new Assignment()
+                {
+                    Title = ipsum.GetWords(4),
+                    Content = ipsum.GetWords(100),
+                    Due = GetRandomDateTime()
+                });
+            }
+        }
+        #endregion
+
+        #region Courses
+        private static List<Course> _courses;
+        public static List<Course> Courses
+        {
+            get { if (_courses == null) GenerateCourses(); return _courses; }
+        }
+        private static void GenerateCourses()
+        {
+            _courses = new List<Course>();
+            for (int i = 0; i < 10; i++)
+            {
+                _courses.Add(new Course()
+                {
+                    Name = ipsum.GetWords(2),
+                    Announcements = new ObservableCollection<Announcement>(Announcements.OrderBy(a => rnd.Next()).Take(rnd.Next(5, 20))),
+                    Assignments = new ObservableCollection<Assignment>(Assignments.OrderBy(a => rnd.Next()).Take(rnd.Next(5, 20))),
+                    Lectures = new ObservableCollection<Lecture>(Lectures.OrderBy(a => rnd.Next()).Take(rnd.Next(5, 20))),
+                    Threads = new ObservableCollection<Thread>(Threads.OrderBy(a => rnd.Next()).Take(rnd.Next(5, 20)))
+                });
+            }
+        }
+        #endregion
+
+        #region Lectures
+        private static List<Lecture> _lectures;
+        public static List<Lecture> Lectures
+        {
+            get { if (_lectures == null) GenerateLectures(); return _lectures; }
+        }
+        private static void GenerateLectures()
+        {
+            _lectures = new List<Lecture>();
+            for (int i = 0; i < 100; i++)
+            {
+                _lectures.Add(new Lecture()
+                {
+                    Title = ipsum.GetWords(4),
+                    Content = ipsum.GetWords(100),
+                    Posted = GetRandomDateTime()
+                });
+            }
+        }
+        #endregion
+
+        #region Posts
+        private static List<Post> _posts;
+        public static List<Post> Posts
+        {
+            get { if (_posts == null) GeneratePosts(); return _posts; }
+        }
+        private static void GeneratePosts()
+        {
+            _posts = new List<Post>();
+            for (int i = 0; i < 100; i++)
+            {
+                _posts.Add(new Post()
+                {
+                    Content = ipsum.GetWords(100),
+                    Posted = GetRandomDateTime(),
+                    Poster = Users[rnd.Next(Users.Count)]
+                });
+            }
+        }
+        #endregion
+
+        #region Threads
+        private static List<Thread> _threads;
+        public static List<Thread> Threads
+        {
+            get { if (_threads == null) GenerateThreads(); return _threads; }
+        }
+        private static void GenerateThreads()
+        {
+            _threads = new List<Thread>();
+            for (int i = 0; i < 100; i++)
+            {
+                _threads.Add(new Thread()
+                {
+                    Title = ipsum.GetWords(4),
+                    Content = ipsum.GetWords(100),
+                    Posted = GetRandomDateTime(),
+                    Posts = new ObservableCollection<Post>(Posts.OrderBy(a => rnd.Next()).Take(rnd.Next(5, 20))),
+                    Poster = Users[rnd.Next(Users.Count)]
+                });
+            }
+        }
+        #endregion
+
+        #region Users
+        private static List<User> _users;
+        public static List<User> Users
+        {
+            get { if (_users == null) GenerateUsers(); return _users; }
+        }
+        private static void GenerateUsers()
+        {
+            _users = new List<User>();
+            for (int i = 0; i < 100; i++)
+            {
+                _users.Add(new User()
+                {
+                    Username = ipsum.GetWords(1)
+                });
+            }
+        }
+        #endregion
+
+        private static DateTime GetRandomDateTime()
+        {
+            DateTime start = new DateTime(2012, 1, 1);
+            int range = (DateTime.Now - start).Days;
+            return start.AddDays(rnd.Next(range));
+        }
+    }
+}
--- CPSC481/ViewModels/MainViewModel.cs
+++ CPSC481/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CPSC481.Models;
+using CPSC481.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -15,7 +16,7 @@ namespace CPSC481.ViewModels
         public MainViewModel()
         {
             Courses = new ObservableCollection<CourseViewModel>();
-            LoadCourses();
+            GenerateData();
             SelectedCourse = Courses[0];
             CurrentUser = new UserViewModel(new User() { Username = "example@example.com" });
         }
@@ -32,28 +33,12 @@ namespace CPSC481.ViewModels
             set { this._currentUser = value; NotifyPropertyChanged("CurrentUser"); }
         }
 
-        private void LoadCourses()
+        private void GenerateData()
         {
-            Course CPSC481 = new Course() { Name = "CPSC 481" };
-            CPSC481.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "Lorem ipsum",Content="The brown fox",Posted=System.DateTime.Now }, new Announcement() { Title = "Dolor sit amet",Content="jumps over",Posted=System.DateTime.Now }, new Announcement() { Title = "Hello world",Content="the lazy dog" }, new Announcement() { Title = "Testing 1,2,3" } };
-            CPSC481.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque",Content = "ullamco laboris nisi" }, new Thread() { Title = "Porta Ornare Venenatis",Content = "ut aliquip ex" }, new Thread() { Title = "Cras",Content="ea commodo consequat" } };
-            CPSC481.Lectures = new ObservableCollection<Lecture> { new Lecture() { Title = "Ligula",Content = "ullamco Ligula" }, new Lecture() { Title="ipsum",Content = "Ultricies Amet Cras" }, new Lecture() { Title="Fringilla Fusce",Content = "Euismod Purus" }, new Lecture() { Title="Lecture4",Content = "Magna" }, new Lecture() { Title="Lecture5",Content = "Vulputate Pharetra" } };
-            CPSC481.Assignments = new ObservableCollection<Assignment> { new Assignment() {Title="One",Content = "Parturient Ipsum",Due=System.DateTime.Now }, new Assignment() { Title="Two",Content = "Fringilla" }, new Assignment() { Title="Three",Content = "Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Title="Four",Content = "Fusce Ligula" }, new Assignment() { Title="Five",Content = "Etiam Ullamcorper" } };
-            Courses.Add(new CourseViewModel(CPSC481));
-
-            Course CPSC457 = new Course() { Name = "CPSC 457" };
-            CPSC457.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "Test ipsum" }, new Announcement() { Title = "Hello world" }, new Announcement() { Title = "Dolor" }, new Announcement() { Title = "Test" } };
-            CPSC457.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque",Content="Content here" }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
-            CPSC457.Lectures = new ObservableCollection<Lecture> { new Lecture() { Content = "Ligula" }, new Lecture() { Content = "Ultricies Amet Cras" }, new Lecture() { Content = "Euismod Purus" }, new Lecture() { Content = "Magna" }, new Lecture() { Content = "Vulputate Pharetra" } };
-            CPSC457.Assignments = new ObservableCollection<Assignment> { new Assignment() { Content = "Parturient Ipsum" }, new Assignment() { Content = "Fringilla" }, new Assignment() { Content = "Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Content = "Fusce Ligula" }, new Assignment() { Content = "Etiam Ullamcorper" } };
-            Courses.Add(new CourseViewModel(CPSC457));
-
-            Course SENG515 = new Course() { Name = "SENG 515" };
-            SENG515.Announcements = new ObservableCollection<Announcement> { new Announcement() { Title = "ipy ipsum" }, new Announcement() { Title = "Hey World" }, new Announcement() { Title = "Hello" }, new Announcement() { Title = "Testing" } };
-            SENG515.Threads = new ObservableCollection<Thread> { new Thread() { Title = "Nullam Pellentesque",Content="words." }, new Thread() { Title = "Porta Ornare Venenatis" }, new Thread() { Title = "Cras" } };
-            SENG515.Lectures = new ObservableCollection<Lecture> { new Lecture() { Content = "Ligula" }, new Lecture() { Content = "Ultricies Amet Cras" }, new Lecture() { Content = "Euismod Purus" }, new Lecture() { Content = "Magna" }, new Lecture() { Content = "Vulputate Pharetra" } };
-            SENG515.Assignments = new ObservableCollection<Assignment> { new Assignment() { Content = "Parturient Ipsum" }, new Assignment() { Content = "Fringilla" }, new Assignment() { Content = "Fermentum Ultricies Fringilla Adipiscing" }, new Assignment() { Content = "Fusce Ligula" }, new Assignment() { Content = "Etiam Ullamcorper" } };
-            Courses.Add(new CourseViewModel(SENG515));
+            foreach (var course in DataGenerator.Courses)
+            {
+                Courses.Add(new CourseViewModel(course));
+            }
         }
     }
 }
